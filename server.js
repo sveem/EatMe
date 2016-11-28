07a84d3050f9d7edcdaf3ab7d386af5d0a07907b
Svetlin Mladenov <svem@gmx.com>
2016-11-28 11:43:07 -0500

    basic server and modules

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var morgan = require('morgan');
+
+var app = express();
+
+// app.use(express.static(__dirname + '/public')); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+  res.send("EatMe is Online");
+})
+
+
+//I have to fix this part before deployment!
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
