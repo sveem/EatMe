2d7607d61518689b84a54458e1cfd8b061c9eb4f
Svetlin Mladenov <svem@gmx.com>
2016-11-29 17:50:37 -0500

    adding bookmarks buttons

--- public/app.js
+++ public/app.js
@@ -1,9 +1,12 @@
- angular.module('EatMe', []) 
+ angular.module('YelpMe', []) 
 
 
 .controller('MainController', function($scope, $http) {
    $scope.data = {};
-   
+   $scope.storage = [];
+   // $scope.bookMarks = {};
+   // $scope.data.model = null;
+
    $scope.printCity = function (city) {
   	console.log("++++++++++++++++++++++++++++++++++CITY>", $scope.data.city)
     return $http({
@@ -16,5 +19,28 @@
        // var parsedData = JSON.parse(returnedData)
        return returnedData;
     });
-  }
+  };
+
+     $scope.bookMark = function() {
+    
+    if($scope.data.food === undefined && $scope.data.city === undefined) {
+      console.log("Add the search queries!");
+    } 
+    else if($scope.data.city === undefined) {
+      console.log("Add a search query to the cities!");
+    }
+    else if ($scope.data.food === undefined) {
+      console.log("Add a search query!");
+    } 
+    else {
+      // $scope.bookMarks.food = $scope.data.food;
+      // $scope.bookMarks.city = $scope.data.city;
+      $scope.storage.push({
+        food: $scope.data.food,
+        city: $scope.data.city
+      });
+      console.log("All search result has been added", $scope.storage);
+    }
+  };
+ 
 });
